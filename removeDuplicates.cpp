09aa8746e67b73ee2480ff160aa31dc2d7d41c73
maverick12345678 <adi.maheshwari53@gmail.com>
2020-02-29 14:55:52 +0530

    README

---
+++ removeDuplicates.cpp
@@ -0,0 +1,18 @@
+/*
+
+Given a sorted array nums, remove the duplicates in-place such that each element
+appear only once and return the new length.
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int removeDuplicates(vector<int> &nums)
+{
+}
+
+int main()
+{
+    return 0;
+}
